90e0b7cd681297efe8575ec42cab5a476c36c045
alex_y <alex@track160.com>
2020-02-20 10:22:41 +0200

    minor

--- cmd_test.cmd
+++ cmd_test.cmd
@@ -1,2 +1,2 @@
 activate violence &^
-python Main.py --train_or_test_mode test --use_pre_trained_model True --pre_trained_model_name 2020-02-13-15_25_59_i3d_soundnet_attention_epoch9_top1_0.801_top5_0.948.pth
+python Main.py --train_or_test_mode test --model_type i3d_soundnet_attention --use_pre_trained_model True --pre_trained_model_name 2020-02-13-15_25_59_i3d_soundnet_attention_epoch9_top1_0.801_top5_0.948.pth
